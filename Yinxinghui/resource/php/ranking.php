4e13506118202892176533564ae959f0f0805e34
ZtianyiKabang <ztianyikabeng@sina.com>
2017-10-20 14:55:31 +0800

    Add files via upload

---
+++ Yinxinghui/resource/php/ranking.php
@@ -0,0 +1,26 @@
+{
+	"status":1,"msg":"提交成功",
+	"data": [{"rnum":"1","name":"test1","phone":"12345678910","time":"1122"},
+	{"rnum":"2","name":"test2","phone":"12345678910","time":"1122"},
+	{"rnum":"3","name":"test3","phone":"12345678910","time":"1122"},
+	{"rnum":"4","name":"test4","phone":"12345678910","time":"1122"},
+	{"rnum":"5","name":"test5","phone":"12345678910","time":"1122"},
+	{"rnum":"1","name":"test1","phone":"12345678910","time":"1121"},
+	{"rnum":"2","name":"test2","phone":"12345678910","time":"1122"},
+	{"rnum":"3","name":"test3","phone":"12345678910","time":"1123"},
+	{"rnum":"4","name":"test4","phone":"12345678910","time":"1124"},
+	{"rnum":"5","name":"test5","phone":"12345678910","time":"1125"},
+	{"rnum":"5","name":"test5","phone":"12345678910","time":"1122"},
+	{"rnum":"1","name":"test1","phone":"12345678910","time":"1121"},
+	{"rnum":"2","name":"test2","phone":"12345678910","time":"1122"},
+	{"rnum":"3","name":"test3","phone":"12345678910","time":"1123"},
+	{"rnum":"4","name":"test4","phone":"12345678910","time":"1124"},
+	{"rnum":"5","name":"test5","phone":"12345678910","time":"1125"},
+	{"rnum":"5","name":"test5","phone":"12345678910","time":"1122"},
+	{"rnum":"1","name":"test1","phone":"12345678910","time":"1121"},
+	{"rnum":"2","name":"test2","phone":"12345678910","time":"1122"},
+	{"rnum":"3","name":"test3","phone":"12345678910","time":"1123"},
+	{"rnum":"4","name":"test4","phone":"12345678910","time":"1124"},
+	{"rnum":"5","name":"test5","phone":"12345678910","time":"1125"}
+	]
+}
